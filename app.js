69ec918c73ab105906ca67c683d988966a95f003
iamabutalha <maabutalhakhalil@gmail.com>
2025-05-17 21:02:56 +0500

    solve issue in promise

---
+++ app.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector(".btn-primary");
+const resultDiv = document.querySelector(".result");
+
+btn.addEventListener("click", () => {
+  const inputValue = document.querySelector("#inputVal");
+
+  fetch(`https://jsonplaceholder.typicode.com/users/${inputValue.value}`)
+    .then((res) => {
+      console.log(res);
+      if (res.status !== 200) {
+        resultDiv.innerHTML = `<h2>No user Found </h2>`;
+        return;
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const HTML = `
+    <h1>${data.name}</h1>
+    <h2>${data.id} </h2>
+    <button class="btnInside">Get User Post </button>
+
+
+    `;
+      resultDiv.innerHTML = HTML;
+
+      let secondBtn = document.querySelector(".btnInside");
+      console.log(secondBtn);
+
+      secondBtn.addEventListener("click", () => {
+        fetch(`https://jsonplaceholder.typicode.com/posts`)
+          .then((res) => res.json())
+          .then((posts) => {
+            console.log(posts);
+            const userPost = posts.filter(
+              (post) => post.userId == inputValue.value
+            );
+            document.querySelector(".postDiv").innerHTML = `<ul>${userPost
+              .map((post) => `<li>${post.title}</li>`)
+              .join("")}</ul>`;
+          });
+      });
+    })
+    .catch((err) => {});
+});
